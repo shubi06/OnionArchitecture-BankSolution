8a92a1d2ae4cc4bec7d29e5dc3cb7bf2c7e94b0b
Shuajb Bunjaku <bunjakushuajb@gmail.com>
2025-03-12 02:03:50 +0100

    Konfigurimet perfundimtare ne Program.cs

--- OnionArchitecure-BankSolution/Presentation/Program.cs
+++ OnionArchitecure-BankSolution/Presentation/Program.cs
@@ -1,22 +1,44 @@
+using Application.Services;
+using Domain.Interfaces;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Shto shërbimet e kontrollerëve
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Konfiguro EF Core me connection string nga appsettings.json
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Regjistro Repository-t
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
+builder.Services.AddScoped<ILocationRepository, LocationRepository>();
+builder.Services.AddScoped<IAllocationRepository, AllocationRepository>();
+
+// Regjistro shërbimet e aplikacionit
+builder.Services.AddScoped<EmployeeService>();
+builder.Services.AddScoped<DepartmentService>();
+// Shtoni edhe shërbime për Project, Location dhe Allocation sipas nevojës
+
+// Shto Swagger për dokumentim API-jeve
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Konfigurimi i middleware-ve
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseRouting();
 
 app.UseAuthorization();
 
